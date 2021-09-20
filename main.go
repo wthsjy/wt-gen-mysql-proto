2ef3b18a6b9d6f30b82c17b0fb870ff48507262a
吴同 <wutong@sagigames.cn>
2021-09-20 17:14:01 +0800

    @

--- main.go
+++ main.go
@@ -27,10 +27,12 @@ var (
 )
 
 func main() {
-	flag.StringVar(&username, "u", "", "用户名,默认为空")
-	flag.StringVar(&password, "p", "", "密码,默认为空")
+	flag.StringVar(&username, "u", "", "用户名")
+	flag.StringVar(&password, "p", "", "密码")
 	flag.StringVar(&host, "h", "127.0.0.1", "主机名,默认 127.0.0.1")
-	flag.IntVar(&port, "P", 3306, "端口号,默认为空")
+	flag.IntVar(&port, "P", 3306, "端口号")
+	flag.StringVar(&dbName, "d", "", "数据库名")
+	flag.StringVar(&tableName, "t", "", "表名")
 
 	// 从arguments中解析注册的flag。必须在所有flag都注册好而未访问其值时执行。未注册却使用flag -help时，会返回ErrHelp。
 	flag.Parse()
@@ -41,19 +43,19 @@ func main() {
 		panic(err)
 	}
 
-	err = mDB.Raw("SHOW FULL FIELDS FROM  user_info").Find(&ddlms).Error
+	err = mDB.Raw(fmt.Sprintf("SHOW FULL FIELDS FROM  %s", tableName)).Find(&ddlms).Error
 	if err != nil {
 		panic(err)
 	}
 
-	protoStr := fmt.Sprintf("message %s{\n", tableName)
+	protoStr := fmt.Sprintf("// 数据库表: %s.%s\nmessage %s{\n", dbName, tableName, FirstUpCase(CamelCase(tableName)))
 	for index, v := range ddlms {
 		if strings.TrimSpace(v.Comment) != "" {
-			protoStr += fmt.Sprintf("	// %s\n", strings.ReplaceAll(strings.TrimSpace(v.Comment), "\n", "\\\\ \n"))
+			protoStr += fmt.Sprintf("  // %s\n", strings.ReplaceAll(strings.TrimSpace(v.Comment), "\n", "\n  // "))
 		}
-		protoStr += fmt.Sprintf("	%s %s = %d;\n", v.Type, v.Field, index)
+		protoStr += fmt.Sprintf("  %s %s = %d; // @gotags: gorm:\"column:%s\"\n", getProtoType(v.Type), CamelCase(v.Field), index+1, v.Field)
 	}
-	protoStr += "\n}"
+	protoStr += "}"
 
 	fmt.Printf("\n\n")
 	fmt.Println(protoStr)
@@ -61,6 +63,62 @@ func main() {
 }
 
 func getDSN() string {
-	dsn := "%s:%s@tcp(%s:%d)/%s"
-	return fmt.Sprintf(dsn, username, password, host, port, dbName)
+	dsnfmt := "%s:%s@tcp(%s:%d)/%s"
+	dsn := fmt.Sprintf(dsnfmt, username, password, host, port, dbName)
+	fmt.Println("dsn:", dsn)
+	return dsn
+}
+
+// TODO 完善更多
+func getProtoType(s string) string {
+	if strings.Contains(s, "bigint") {
+		if strings.Contains(s, "unsigned") {
+			return "uint64"
+		} else {
+			return "int64"
+		}
+	}
+	if strings.Contains(s, "int") {
+		if strings.Contains(s, "unsigned") {
+			return "uint32"
+		} else {
+			return "int32"
+		}
+	}
+	return "string"
+}
+
+func CamelCase(s string) string {
+	var b []byte
+	var wasUnderscore bool
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if c != '_' {
+			if wasUnderscore && isASCIILower(c) {
+				c -= 'a' - 'A'
+			}
+			b = append(b, c)
+		}
+		wasUnderscore = c == '_'
+	}
+	return string(b)
+}
+
+func isASCIILower(c byte) bool {
+	return 'a' <= c && c <= 'z'
+}
+
+func FirstUpCase(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	if !isASCIILower(s[0]) {
+		return s
+	}
+	c := s[0]
+	c -= 'a' - 'A'
+	b := []byte(s)
+	b[0] = c
+	return string(b)
 }
