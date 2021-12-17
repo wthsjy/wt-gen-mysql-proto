e6f2c8efefc55884203be84ea7accb6c39f9d7a6
wutong <wutong@doufan.tv>
2021-12-17 11:46:46 +0800

    @

--- main.go
+++ main.go
@@ -29,12 +29,12 @@ var (
 )
 
 func main() {
-	flag.StringVar(&username, "u", "", "用户名")
-	flag.StringVar(&password, "p", "", "密码")
+	flag.StringVar(&username, "u", "root", "用户名")
+	flag.StringVar(&password, "p", "root", "密码")
 	flag.StringVar(&host, "h", "127.0.0.1", "主机名,默认 127.0.0.1")
 	flag.IntVar(&port, "P", 3306, "端口号")
-	flag.StringVar(&dbName, "d", "", "数据库名")
-	flag.StringVar(&tableName, "t", "", "表名")
+	flag.StringVar(&dbName, "d", "demo", "数据库名")
+	flag.StringVar(&tableName, "t", "user_info", "表名")
 
 	// 从arguments中解析注册的flag。必须在所有flag都注册好而未访问其值时执行。未注册却使用flag -help时，会返回ErrHelp。
 	flag.Parse()
@@ -51,42 +51,45 @@ func main() {
 	}
 
 	var fields []string
-	//var priKeyField string
-	//structStr := fmt.Sprintf("package dbmodel\n\n// %s mysql database.table: %s.%s\ntype %s struct{\n", FirstUpCase(CamelCase(tableName)), dbName, tableName, FirstUpCase(CamelCase(tableName)))
+	var priKeyField string
+	structStr := fmt.Sprintf("package dbmodel\n\n// %s mysql database.table: %s.%s\ntype %s struct{\n", FirstUpCase(CamelCase(tableName)), dbName, tableName, FirstUpCase(CamelCase(tableName)))
 	protoStr := "syntax = \"proto3\";\n\n"
 	protoStr += "package dodo.go.pbgen.service.model;\n"
 	protoStr += "option go_package = \"dodo-go/pbgen/service/model;modelpb\";\n\n"
 	protoStr += fmt.Sprintf("// mysql database.table: %s.%s\nmessage %s{\n", dbName, tableName, FirstUpCase(CamelCase(tableName)))
 	for index, v := range ddlms {
 		fields = append(fields, fmt.Sprintf("`%s`", v.Field))
-		//if strings.ToUpper(v.Key) == "PRI" {
-		//	priKeyField = v.Field
-		//}
+		if strings.ToUpper(v.Key) == "PRI" {
+			priKeyField = v.Field
+		}
 		if strings.TrimSpace(v.Comment) != "" {
 			protoStr += fmt.Sprintf("  // %s\n", strings.ReplaceAll(strings.TrimSpace(v.Comment), "\n", "\n  // "))
 		}
 		protoStr += fmt.Sprintf("  //\n  // table field:\n  // @gotags: gorm:\"column:%s\"\n", v.Field)
 		protoStr += fmt.Sprintf("  %s %s = %d;\n", getProtoType(v.Type), CamelCase(v.Field), index+1)
 
-		//if strings.TrimSpace(v.Comment) != "" {
-		//	structStr += fmt.Sprintf("  // %s\n", strings.ReplaceAll(strings.TrimSpace(v.Comment), "\n", "\n  // "))
-		//}
-		//structStr += fmt.Sprintf("  %s %s  ", FirstUpCase(CamelCase(v.Field)), getProtoType(v.Type))
-		//structStr += fmt.Sprintf("`json:\"%s\" gorm:\"column:%s\"`\n", CamelCase(v.Field), v.Field)
+		if strings.TrimSpace(v.Comment) != "" {
+			structStr += fmt.Sprintf("  // %s\n", strings.ReplaceAll(strings.TrimSpace(v.Comment), "\n", "\n  // "))
+		}
+		structStr += fmt.Sprintf("  %s %s  ", FirstUpCase(CamelCase(v.Field)), getProtoType(v.Type))
+		structStr += fmt.Sprintf("`json:\"%s\" gorm:\"column:%s\"`\n", CamelCase(v.Field), v.Field)
 	}
 
 	protoStr += "}"
 
-	//structStr += "}\n\n"
-	//structStr += "const(\n"
-	//structStr += fmt.Sprintf("  sqlAdd%s =\"insert into `%s`(%s)values(%s)\"\n", FirstUpCase(CamelCase(tableName)), tableName, strings.Join(fields, ","), strings.Join(strings.Split(strings.Repeat("?", len(fields)), ""), ","))
-	//structStr += fmt.Sprintf("  sqlDel%sByIds =\"delete from `%s` where `%s` in ?\"\n", FirstUpCase(CamelCase(tableName)), tableName, priKeyField)
-	//structStr += fmt.Sprintf("  sqlGet%sByIds =\"select %s from `%s` where `%s` in ?\"\n", FirstUpCase(CamelCase(tableName)), strings.Join(fields, ","), tableName, priKeyField)
-	//structStr += ")"
+	structStr += "}\n\n"
+	structStr += "const(\n"
+	structStr += fmt.Sprintf("  sqlAdd%s =\"insert into `%s`(%s)values(%s)\"\n", FirstUpCase(CamelCase(tableName)), tableName, strings.Join(fields, ","), strings.Join(strings.Split(strings.Repeat("?", len(fields)), ""), ","))
+	structStr += fmt.Sprintf("  sqlDel%sByIds =\"delete from `%s` where `%s` in ?\"\n", FirstUpCase(CamelCase(tableName)), tableName, priKeyField)
+	structStr += fmt.Sprintf("  sqlGet%sByIds =\"select %s from `%s` where `%s` in ?\"\n", FirstUpCase(CamelCase(tableName)), strings.Join(fields, ","), tableName, priKeyField)
+	structStr += ")"
 
-	protoFileName := fmt.Sprintf("./proto/service/model/%s.model.proto", tableName)
+	protoFileName := fmt.Sprintf("./%s.model.proto", tableName)
+	structFileName := fmt.Sprintf("./%s.model.go", tableName)
 	_ = ioutil.WriteFile(protoFileName, []byte(protoStr), 0666)
+	_ = ioutil.WriteFile(structFileName, []byte(structStr), 0666)
 	fmt.Println("proto:", protoFileName)
+	fmt.Println("struct:", structFileName)
 }
 
 func getDSN() string {
@@ -107,10 +110,13 @@ func getProtoType(s string) string {
 	}
 	if strings.Contains(s, "int") {
 		if strings.Contains(s, "unsigned") {
-			return "uint32"
+			return "uint"
 		} else {
-			return "int32"
+			return "int"
 		}
+	}
+	if strings.Contains(strings.ToLower(s), "datetime") {
+		return "time.Time"
 	}
 	return "string"
 }
